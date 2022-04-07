11a0072ee8f8645f49e55280e832053610a76dae
Lim Jihwan <ghwn5936@gmail.com>
2022-04-08 00:31:11 +0900

    Nginx upstream daphne

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['app']
+ALLOWED_HOSTS = ['localhost']
 
 DATABASES = {
     'default': {
