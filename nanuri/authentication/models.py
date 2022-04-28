eaa645bffedb8014d675eea200a31d3074881500
Lim Jihwan <ghwn5936@gmail.com>
2022-04-28 23:34:20 +0900

    Make fields non-nullable

--- nanuri/authentication/models.py
+++ nanuri/authentication/models.py
@@ -3,17 +3,7 @@ from django.db import models
 
 
 class KakaoAccount(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        default=None,
-    )
-    kakao_id = models.PositiveBigIntegerField(
-        null=True,
-        blank=True,
-        default=None,
-    )
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    kakao_id = models.PositiveBigIntegerField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
