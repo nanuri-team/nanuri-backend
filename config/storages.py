13a31bad6c78d1ceb648ba10fe0c5a60ee4a3c8c
Lim Jihwan <ghwn5936@gmail.com>
2022-05-06 18:57:03 +0900

    Fix nginx container and storage settings

---
+++ config/storages.py
@@ -0,0 +1,9 @@
+from storages.backends.s3boto3 import S3Boto3Storage, S3StaticStorage
+
+
+class StaticStorage(S3StaticStorage):
+    location = 'static'
+
+
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
