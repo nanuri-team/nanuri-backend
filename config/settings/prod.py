23e74d5e6410d45196b2e3341631bfb10e26b62a
Lim Jihwan <ghwn5936@gmail.com>
2022-05-06 16:55:44 +0900

    Collect staticfiles to S3

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -36,6 +36,7 @@ CHANNEL_LAYERS = {
 AWS_DYNAMODB_ENDPOINT_URL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID']
