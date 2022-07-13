fd49648348c659c2c339a4c7005f3885da3fac16
Lim Jihwan <ghwn5936@gmail.com>
2022-07-13 17:24:15 +0900

    Add post api url tests

---
+++ tests/posts/test_api_urls.py
@@ -0,0 +1,70 @@
+import pytest
+from django.urls import reverse
+
+pytestmark = pytest.mark.django_db
+
+
+class TestApiUrls:
+    base_url = "/api/v1"
+
+    def test_post_list_url(self):
+        url = reverse("nanuri.posts.api:list")
+        assert url == self.base_url + "/posts/"
+
+    def test_post_detail_url(self, post):
+        url = reverse("nanuri.posts.api:detail", kwargs={"uuid": post.uuid})
+        assert url == self.base_url + f"/posts/{post.uuid}/"
+
+    def test_post_image_list_url(self, post):
+        url = reverse("nanuri.posts.api:image-list", kwargs={"uuid": post.uuid})
+        assert url == self.base_url + f"/posts/{post.uuid}/images/"
+
+    def test_post_image_detail_url(self, post):
+        filename = "test.jpg"
+        url = reverse(
+            "nanuri.posts.api:image-detail",
+            kwargs={
+                "uuid": post.uuid,
+                "filename": filename,
+            },
+        )
+        assert url == self.base_url + f"/posts/{post.uuid}/images/{filename}/"
+
+    def test_comment_list_urls(self, post):
+        url = reverse("nanuri.posts.api:comment-list", kwargs={"uuid": post.uuid})
+        assert url == self.base_url + f"/posts/{post.uuid}/comments/"
+
+    def test_comment_detail_urls(self, comment):
+        post = comment.post
+
+        url = reverse(
+            "nanuri.posts.api:comment-detail",
+            kwargs={"uuid": post.uuid, "comment_uuid": comment.uuid},
+        )
+        assert url == self.base_url + f"/posts/{post.uuid}/comments/{comment.uuid}/"
+
+    def test_sub_comment_list_urls(self, comment):
+        post = comment.post
+
+        url = reverse(
+            "nanuri.posts.api:sub-comment-list",
+            kwargs={
+                "uuid": post.uuid,
+                "comment_uuid": comment.uuid,
+            },
+        )
+        assert url == self.base_url + f"/posts/{post.uuid}/comments/{comment.uuid}/sub-comments/"
+
+    def test_sub_comment_detail_urls(self, sub_comment):
+        post = sub_comment.comment.post
+        comment = sub_comment.comment
+
+        url = reverse(
+            "nanuri.posts.api:sub-comment-detail",
+            kwargs={
+                "uuid": post.uuid,
+                "comment_uuid": comment.uuid,
+                "sub_comment_uuid": sub_comment.uuid,
+            },
+        )
+        assert url == self.base_url + f"/posts/{post.uuid}/comments/{comment.uuid}/sub-comments/{sub_comment.uuid}/"
