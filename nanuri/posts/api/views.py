06fd6cadde73abdd51050350a2d87f53e9249504
Lim Jihwan <ghwn5936@gmail.com>
2022-08-08 19:59:07 +0900

    Update api docs

--- nanuri/posts/api/views.py
+++ nanuri/posts/api/views.py
@@ -1,5 +1,10 @@
 from django.core.files.storage import default_storage
-from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view
+from drf_spectacular.utils import (
+    OpenApiExample,
+    OpenApiParameter,
+    extend_schema,
+    extend_schema_view,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import LimitOffsetPagination
@@ -25,125 +30,59 @@ from .serializers import CommentSerializer, PostSerializer, SubCommentSerializer
         ],
     ),
     post=extend_schema(
-        description="""<h2>상품 정보를 등록하는 API</h2>
-            <h3>
-            - title : 상품 게시글의 제목 <br><br>
-            - image : 상품 이미지 파일 등록 <br><br>
-            - category : 상품의 카테고리 (생활용품, 음식, 주방, 욕실, 문구, 기타) <br><br>
-            - unit_price : 상품의 가격 <br><br>
-            - quantity :  상품의 수량 <br><br>
-            - description : 상품에 대한 설명 <br><br>
-            - min_participants : 최소 참가 인원 <br><br>
-            - max_participants : 최대 참가 인원 <br><br>
-            - num_participants : 현재 참가 인원  <br><br>
-            - product_url : 상품 URL <br><br>
-            - trade_type : 나눔 방법 (DIRECT(직거래), PARCEL(택배 거래)) <br><br>
-            - order_status : 상품의 진행상태 (WAITING(인원 모집 중), ORDERING(주문 진행 중), ORDERED(주문 완료), DELIVERING1(1차 배송 중), DELIVERING2(2차 배송 중), DELIVERED(배송 완료), CANCELLED(취소됨)) <br><br>
-            - waited_from : 참가자 모집 시작일 <br><br>
-            - waited_until : 참가자 모집 마지막일
-            </h3>
-        """,
+        description="<h2>글을 생성하는 API</h2>",
         summary="Create a new post",
         tags=["Post"],
-        request={
-            "multipart/form-data": {
-                "type": "object",
-                "properties": {
-                    "title": {
-                        "type": "string",
-                        "default": "나누리 상품 공동구매 인원 모집합니다.",
-                        "required": True,
-                        "nullable": False,
-                    },
-                    "image": {
-                        "type": "file",
-                        "format": "formData",
-                        "required": False,
-                    },
-                    "category": {
-                        "type": "string",
-                        "formData": "enum",
-                        "enum": [
-                            "생활용품",
-                            "음식",
-                            "주방",
-                            "욕실",
-                            "문구",
-                            "기타",
-                        ],
-                    },
-                    "unit_price": {
-                        "type": "integer",
-                        "default": "8000",
-                        "required": True,
-                    },
-                    "quantity": {
-                        "type": "integer",
-                        "default": "6",
-                        "required": True,
-                    },
-                    "description": {
-                        "type": "string",
-                        "default": "같이 공동구매 하실분?",
-                        "required": True,
-                    },
-                    "min_participants": {
-                        "type": "integer",
-                        "format": "Int",
-                        "default": "3",
-                        "required": True,
-                    },
-                    "max_participants": {
-                        "type": "integer",
-                        "format": "Int",
-                        "default": "6",
-                        "required": True,
-                    },
-                    "product_url": {
-                        "type": "string",
-                        "format": "url",
-                        "default": "http://localhost:8080/admin/",
-                        "required": True,
-                    },
-                    "trade_type": {
-                        "type": "string",
-                        "formData": "enum",
-                        "enum": ["DIRECT", "PARCEL"],
-                    },
-                    "order_status": {
-                        "type": "string",
-                        "formData": "enum",
-                        "enum": [
-                            "WAITING",
-                            "ORDERING",
-                            "ORDERED",
-                            "DELIVERING1",
-                            "DELIVERING2",
-                            "DELIVERED",
-                            "CANCELLED",
-                        ],
-                        "default": "WAITING",
-                    },
-                    "is_published": {
-                        "type": "boolean",
-                        "default": "true",
-                    },
-                    "view_count": {
-                        "type": "integer",
-                        "readOnly": True,
-                    },
-                    "waited_from": {
-                        "type": "date",
-                        "formData": "date",
-                        "default": "2022-04-30",
-                    },
-                    "waited_until": {
-                        "type": "date",
-                        "default": "2022-05-30",
-                    },
+        examples=[
+            OpenApiExample(
+                name="Request #1",
+                value={
+                    "title": "콜라 공동구매합니다",
+                    "unit_price": 1500,
+                    "quantity": 30,
+                    "description": "좋은 가격에 콜라 같이 구매하실 분~",
+                    "min_participants": 10,
+                    "max_participants": 50,
+                    "product_url": "https://example.com/items/coke",
+                    "category": "FOOD",
+                    "trade_type": "DIRECT",
+                    "waited_until": "2022-08-05",
                 },
-            },
-        },
+                request_only=True,
+            ),
+            OpenApiExample(
+                name="Response #1",
+                value={
+                    "writer": "nanuri@nanuri.app",
+                    "writer_address": "나누리시 나누리구 나누리동 나누리아파트 101동 101호",
+                    "writer_nickname": "나누리",
+                    "participants": ["nanuri@nanuri.app"],
+                    "favored_by": [],
+                    "images": [],
+                    "uuid": "b6accf2d-f527-4096-bcf2-3a18198db198",
+                    "title": "콜라 공동구매합니다",
+                    "image": None,
+                    "unit_price": 1500,
+                    "quantity": 30,
+                    "description": "좋은 가격에 콜라 같이 구매하실 분~",
+                    "category": "FOOD",
+                    "min_participants": 10,
+                    "max_participants": 50,
+                    "num_participants": 0,
+                    "product_url": "https://nanuri.app/items/coke",
+                    "trade_type": "DIRECT",
+                    "order_status": "WAITING",
+                    "is_published": True,
+                    "published_at": "2022-08-02T13:13:08.095902Z",
+                    "view_count": 0,
+                    "waited_from": "2022-08-02",
+                    "waited_until": "2022-08-05",
+                    "created_at": "2022-08-02T13:13:08.309620Z",
+                    "updated_at": "2022-08-02T13:13:08.309642Z",
+                },
+                response_only=True,
+            ),
+        ],
     ),
 )
 class PostListCreateAPIView(ListCreateAPIView):
