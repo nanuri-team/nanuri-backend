e46fe119765e3b48637533ebe6da706d879d6df7
Lim Jihwan <ghwn5936@gmail.com>
2022-08-20 04:30:38 +0900

    Revert "Update ci setting"
    This reverts commit a7cd4422a96cb2207a4686522d9bae0e0435ab09.

--- localstack-setup.sh
+++ localstack-setup.sh
@@ -3,5 +3,5 @@
 # .env -> AWS_S3_CERTIFICATE_BUCKET_NAME
 # .env -> AWS_S3_CERTIFICATE_KEY
 awslocal s3api create-bucket --bucket nanuri-secret-bucket
-wget https://github.com/aryansbtloe/resources/raw/master/Certificate/certificate.p12 -O /tmp/certificate.p12
-awslocal s3api put-object --bucket nanuri-secret-bucket --key certificate.p12 --body /tmp/certificate.p12
+wget https://github.com/aryansbtloe/resources/raw/master/Certificate/certificate.p12 -O ./certificate.p12
+awslocal s3api put-object --bucket nanuri-secret-bucket --key certificate.p12 --body certificate.p12
