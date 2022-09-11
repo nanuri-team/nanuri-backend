59dc5dcb679ed6371ef7819ee3a55b8263122368
Lim Jihwan <ghwn5936@gmail.com>
2022-09-11 20:02:43 +0900

    Make email unique

--- tests/users/factories.py
+++ tests/users/factories.py
@@ -7,7 +7,7 @@ from factory.fuzzy import FuzzyChoice
 class UserFactory(DjangoModelFactory):
     class Meta:
         model = get_user_model()
-        django_get_or_create = ("email", "nickname")
+        django_get_or_create = ("email",)
 
     email = factory.Faker("email")
     password = factory.Faker("password")
