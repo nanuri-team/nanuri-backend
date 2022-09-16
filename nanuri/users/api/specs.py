f2b5cd417a4cbb9c86311e82d0db4effff5409a3
Lim Jihwan <ghwn5936@gmail.com>
2022-09-16 14:27:27 +0900

    User API 문서 수정

--- nanuri/users/api/specs.py
+++ nanuri/users/api/specs.py
@@ -7,31 +7,30 @@ users_api_specs = {
         tags=["User"],
     ),
     "post": extend_schema(
-        description="""<h2>회원 정보를 등록하는 API</h2>
-            <h3>
-            - email : 사용자의 email <br><br>
-            - nickname : 사용자의 닉네임 <br><br>
-            - is_active : 계정의 활성화 유무 <br><br>
-            - is_admin :  admin 권한 제공 유무 <br><br>
-            - address : 사용자의 주소 <br><br>
-            - profile_url : 사용자의 프로필 이미지 url  <br><br>
-            - auth_provider : 소셜로그인 종류 (APPLE, KAKAO) <br><br>
-            </h3>
-        """,
+        description="<h2>회원 정보를 등록하는 API</h2>",
         summary="Create a new user",
         tags=["User"],
         examples=[
             OpenApiExample(
-                name="success_example",
+                name="example_all_fields",
                 value={
-                    "password": "null",
-                    "email": "nanuri@example.com",
-                    "nickname": "nanuri",
-                    "is_active": "true",
-                    "is_admin": "false",
-                    "address": "나누리시 나누리구 나누리동",
-                    "profile_url": "https://nanuri.app/",
-                    "auth_provider": "APPLE",
+                    "email": "michaelmontoya@example.com",
+                    "password": "uG5*(GNr&F",
+                    "nickname": "michael",
+                    "is_active": True,
+                    "is_admin": False,
+                    "address": "9197 Turner Roads\nNorth Melissaberg, MT 83503",
+                    "profile": "https://dummyimage.com/2x721.jpeg",
+                    "auth_provider": "KAKAO",
+                    "location": "SRID=4326;POINT (-22.86 1.2281245)",
+                },
+                description="location 포맷은 `SRID=4326;POINT (경도 위도)`입니다.",
+            ),
+            OpenApiExample(
+                name="example_required_fields_only",
+                value={
+                    "email": "michaelmontoya@example.com",
+                    "password": "uG5*(GNr&F",
                 },
             ),
         ],
@@ -46,63 +45,35 @@ user_api_specs = {
         tags=["User"],
     ),
     "put": extend_schema(
-        description="""<h2>회원 정보 전체를 업데이트 하는 API</h2>
-            ! 전체 필드에 대해서 update
-            <h3>
-            - email : 사용자의 email <br><br>
-            - nickname : 사용자의 닉네임 <br><br>
-            - is_active : 계정의 활성화 유무 <br><br>
-            - is_admin :  admin 권한 제공 유무 <br><br>
-            - address : 사용자의 주소 <br><br>
-            - profile_url : 사용자의 프로필 이미지 url  <br><br>
-            - auth_provider : 소셜로그인 종류 (APPLE, KAKAO) <br><br>
-            </h3>
-        """,
+        description="<h2>회원 정보 전체를 업데이트 하는 API</h2>",
         summary="Update user",
         tags=["User"],
         examples=[
             OpenApiExample(
                 name="success_example",
                 value={
-                    "password": "null",
-                    "email": "nanuri@example.com",
-                    "nickname": "nanuri",
-                    "is_active": "true",
-                    "is_admin": "false",
-                    "address": "나누리시 나누리구 나누리동",
-                    "profile_url": "https://nanuri.app/",
-                    "auth_provider": "APPLE",
+                    "email": "michaelmontoya@example.com",
+                    "password": "uG5*(GNr&F",
+                    "nickname": "michael",
+                    "is_active": True,
+                    "is_admin": False,
+                    "address": "9197 Turner Roads\nNorth Melissaberg, MT 83503",
+                    "profile": "https://dummyimage.com/2x721.jpeg",
+                    "auth_provider": "KAKAO",
+                    "location": "SRID=4326;POINT (-22.86 1.2281245)",
                 },
             ),
         ],
     ),
     "patch": extend_schema(
-        description="""<h2>회원 정보를 업데이트 하는 API</h2>
-            ! 필요한 필드만 update
-            <h3>
-            - email : 사용자의 email <br><br>
-            - nickname : 사용자의 닉네임 <br><br>
-            - is_active : 계정의 활성화 유무 <br><br>
-            - is_admin :  admin 권한 제공 유무 <br><br>
-            - address : 사용자의 주소 <br><br>
-            - profile_url : 사용자의 프로필 이미지 url  <br><br>
-            - auth_provider : 소셜로그인 종류 (APPLE, KAKAO) <br><br>
-            </h3>
-        """,
+        description="<h2>회원 정보를 부분 수정하는 API</h2>",
         summary="Update user",
         tags=["User"],
         examples=[
             OpenApiExample(
                 name="success_example",
                 value={
-                    "password": "null",
-                    "email": "nanuri@example.com",
-                    "nickname": "nanuri",
-                    "is_active": "true",
-                    "is_admin": "false",
-                    "address": "나누리시 나누리구 나누리동",
-                    "profile_url": "https://nanuri.app/",
-                    "auth_provider": "APPLE",
+                    "location": "SRID=4326;POINT (-22.86 1.2281245)",
                 },
             ),
         ],
