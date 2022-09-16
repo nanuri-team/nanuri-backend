9a0e055f0d346fa93766490b3f84daf255c951ec
Lim Jihwan <ghwn5936@gmail.com>
2022-09-16 14:31:33 +0900

    Token API URL 테스트 추가

--- tests/authentication/api/test_api_urls.py
+++ tests/authentication/api/test_api_urls.py
@@ -10,3 +10,15 @@ class TestApiUrls:
     def test_user_list_url(self):
         url = reverse("nanuri.authentication:kakao-account-list")
         assert url == self.base_url + "/auth/kakao/accounts/"
+
+    def test_token_url(self):
+        url = reverse("nanuri.authentication:token_obtain_pair")
+        assert url == self.base_url + "/auth/token/"
+
+    def test_token_refresh_url(self):
+        url = reverse("nanuri.authentication:token_refresh")
+        assert url == self.base_url + "/auth/token/refresh/"
+
+    def test_token_verify_url(self):
+        url = reverse("nanuri.authentication:token_verify")
+        assert url == self.base_url + "/auth/token/verify/"
