f01660c9c5955cdcb48ba8c58500fabacd24fad6
Lim Jihwan <ghwn5936@gmail.com>
2022-11-05 13:06:50 +0900

    Subscription 생성 전 검증

--- tests/notifications/test_device_api.py
+++ tests/notifications/test_device_api.py
@@ -2,7 +2,7 @@ import pytest
 from django.urls import reverse
 
 from nanuri.aws.sns import sns
-from nanuri.notifications.models import Device
+from nanuri.notifications.models import Device, Subscription
 
 from .factories import DeviceFactory, SubscriptionFactory
 
@@ -97,13 +97,22 @@ class TestDeviceApi:
         else:
             assert endpoint is None
 
-    def test_delete(self, user_client, device):
-        child_subscriptions = SubscriptionFactory.create_batch(size=3, device=device)
-        assert len(device.subscription_set.all()) == 3
-
-        assert sns.get_endpoint_by_device_token(device.device_token) is not None
+    @pytest.mark.parametrize("subscription_opt_in", [True, False])
+    @pytest.mark.parametrize("device_opt_in", [True, False])
+    def test_delete(self, user_client, device_opt_in, subscription_opt_in):
+        # given: 1개의 기기과 3개의 구독을 생성함
+        device = DeviceFactory.create(opt_in=device_opt_in)
+        subscriptions = SubscriptionFactory.create_batch(
+            size=3,
+            device=device,
+            opt_in=subscription_opt_in,
+        )
+        device_token = device.device_token
+        subscription_arns = [s.subscription_arn for s in subscriptions]
         assert Device.objects.all().count() == 1
+        assert Subscription.objects.filter(device=device).count() == 3
 
+        # when: 기기를 삭제함
         response = user_client.delete(
             reverse(
                 "nanuri.notifications.api:device-detail",
@@ -112,9 +121,11 @@ class TestDeviceApi:
         )
         assert response.status_code == 204
 
+        # then: 기기, 구독 정보 모두 삭제되어야 함
         assert Device.objects.all().count() == 0
-        assert sns.get_endpoint_by_device_token(device.device_token) is None
+        assert Subscription.objects.filter(device=device).count() == 0
 
-        subscriptions = [x["SubscriptionArn"] for x in sns.list_subscriptions()]
-        for child_subscription in child_subscriptions:
-            assert child_subscription.subscription_arn not in subscriptions
+        assert sns.get_endpoint_by_device_token(device_token) is None
+        all_subscriptions = [x["SubscriptionArn"] for x in sns.list_subscriptions()]
+        for subscription_arn in subscription_arns:
+            assert subscription_arn not in all_subscriptions
